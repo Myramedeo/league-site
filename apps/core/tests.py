ec8491af1991f8ab22fe8036ac2fb6277f4dd24b
Roland Kim <103455333+Myramedeo@users.noreply.github.com>
2026-07-17 15:19:51 -0400

    Homepage fix

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -3,8 +3,10 @@ from django.urls import reverse
 
 
 class HomePageTests(TestCase):
-    def test_homepage_uses_a_dedicated_template(self):
+    def test_homepage_renders_without_template_errors(self):
         response = self.client.get(reverse('home'))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'core/home.html')
+        self.assertContains(response, 'Welcome to')
+        self.assertContains(response, 'Current Season')
