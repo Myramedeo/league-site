5ea6784cbc2b63e5023606cf08f0d327115a0d23
Roland Kim <103455333+Myramedeo@users.noreply.github.com>
2026-09-07 18:04:50 -0400

    Playoffs in admin panel

--- apps/teams/admin.py
+++ apps/teams/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
-from .models import Season, Team
+from .models import Competition, Season, Team
+
+
+class CompetitionInline(admin.TabularInline):
+    model = Competition
+    extra = 0
+    fields = ('name', 'phase', 'source_order', 'expected_team_count')
+
 
 @admin.register(Season)
 class SeasonAdmin(admin.ModelAdmin):
     list_display = ('year', 'name')
     ordering = ('-year',)
+    inlines = (CompetitionInline,)
+
+
+@admin.register(Competition)
+class CompetitionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'season', 'phase', 'source_order')
+    list_filter = ('season', 'phase')
+    search_fields = ('name',)
+    ordering = ('-season__year', 'source_order', 'name')
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
